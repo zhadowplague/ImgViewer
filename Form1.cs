16fbaaf61acf4ab43ece011b74e87b2bcf7470ec
BossBin <albin.hos@hotmail.com>
2025-02-06 20:44:53 +0100

    add mouse drag feature

--- Form1.cs
+++ Form1.cs
@@ -19,6 +19,11 @@ namespace ImgViewer
 		float _zoom = 1;
 		float _zoomFactor = 0.1f;
 		bool _slowZoom = false;
+		bool _mouseDown = false;
+		bool _filter = false;
+		PointF _offset = PointF.Empty;
+		Point _position = Point.Empty;
+		DateTime _lastClick;
 
 		public Form1(string[] args)
 		{
@@ -34,6 +39,47 @@ namespace ImgViewer
 			pictureBox1.MouseWheel += PictureBox1_MouseWheel;
 			pictureBox1.Paint += PictureBox1_Paint;
 			this.KeyUp += Form1_KeyUp;
+			pictureBox1.MouseDown += Form1_MouseDown;
+			this.MouseDown += Form1_MouseDown;
+			pictureBox1.MouseUp += Form1_MouseUp;
+			this.MouseUp += Form1_MouseUp;
+			pictureBox1.MouseMove += Form1_MouseMove;
+		}
+
+		private void Form1_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (!_mouseDown || _position ==  Point.Empty)
+			{
+				_position = e.Location;
+				return;
+			}
+
+			_offset.X += e.Location.X - _position.X;
+			_offset.Y += e.Location.Y - _position.Y;
+			_position = e.Location;
+			pictureBox1.Invalidate();
+		}
+
+		private void Form1_MouseUp(object sender, MouseEventArgs e)
+		{
+			if (DateTime.UtcNow.Subtract(_lastClick).TotalSeconds < 0.2)
+			{
+				if (e.Location.X < Size.Width * 0.15)
+				{
+					PreviousImg();
+				}
+				else if (e.Location.X > Size.Width * 0.85)
+				{
+					NextImg();
+				}
+			}
+			_mouseDown = false;
+		}
+
+		private void Form1_MouseDown(object sender, MouseEventArgs e)
+		{
+			_mouseDown = true;
+			_lastClick = DateTime.UtcNow;
 		}
 
 		private void Form1_KeyUp(object sender, KeyEventArgs e)
@@ -44,41 +90,55 @@ namespace ImgViewer
 			}
 		}
 
+		private float Clamp(float target, float min, float max) => Math.Min(max, Math.Max(min, target));
+
 		private void PictureBox1_Paint(object sender, PaintEventArgs e)
 		{
 			e.Graphics.Clear(this.BackColor);
-			e.Graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+			e.Graphics.InterpolationMode = _filter ? InterpolationMode.HighQualityBicubic : InterpolationMode.NearestNeighbor;
 			e.Graphics.PixelOffsetMode = PixelOffsetMode.None;
 			var width = pictureBox1.Image.Size.Width * _zoom;
 			var height = pictureBox1.Image.Size.Height * _zoom;
-			var x = pictureBox1.Size.Width * 0.5f - (width * 0.5f);
-			var y = pictureBox1.Size.Height * 0.5f - (height * 0.5f);
+			_offset.X = Clamp(_offset.X, -width * 0.5f, width * 0.5f);
+			_offset.Y = Clamp(_offset.Y, -height * 0.5f, height * 0.5f);
+			var x = pictureBox1.Size.Width * 0.5f - (width * 0.5f) + _offset.X;
+			var y = pictureBox1.Size.Height * 0.5f - (height * 0.5f) + _offset.Y;
 			e.Graphics.DrawImage(pictureBox1.Image, new RectangleF(x, y, width, height));
 		}
 
 		private void PictureBox1_MouseWheel(object sender, MouseEventArgs e)
 		{
 			var zoomSpeed = _slowZoom ? _zoomFactor * 0.1f : _zoomFactor;
-			SetZoom(_zoom + (e.Delta * zoomSpeed));
+			SetZoom(_zoom + (e.Delta * zoomSpeed), e.Location);
 		}
 
-		public void SetZoom(float zoomfactor)
+		public void SetZoom(float zoomfactor, Point mousePos)
 		{
 			if (zoomfactor <= 0) 
 				return;
+
+			float oldZoom = _zoom;
 			_zoom = zoomfactor;
+
+			var imageX = (mousePos.X - pictureBox1.Width * 0.5f + _offset.X) / oldZoom;
+			var imageY = (mousePos.Y - pictureBox1.Height * 0.5f + _offset.Y) / oldZoom;
+			_offset.X += (imageX * (oldZoom - _zoom));
+			_offset.Y += (imageY * (oldZoom - _zoom));
+
 			pictureBox1.Invalidate();
 		}
 
 		private void LoadImg()
 		{
+			_offset = PointF.Empty;
 			try
 			{
 				pictureBox1.Image = Image.FromFile(_path);
 				var imgMax = (float)Math.Max(pictureBox1.Image.Width, pictureBox1.Image.Height);
 				var windowMax = (float)Math.Max(pictureBox1.Size.Width, pictureBox1.Size.Height);
 				_zoomFactor = 1.0f / imgMax;
-				SetZoom((windowMax * 0.5f) / imgMax);
+				_zoom = (windowMax * 0.5f) / imgMax;
+				pictureBox1.Invalidate();
 			}
 			catch 
 			{
@@ -97,37 +157,51 @@ namespace ImgViewer
 		{
 			if (e.KeyCode == Keys.Left)
 			{
-				var files = GetFilesInFolder();
-				var index = Array.IndexOf(files, _path);
-				while (index > 0)
-				{
-					index--;
-					var extension = Path.GetExtension(files[index]);
-					if (!allowedExtensions.Contains(extension))
-						continue;
-					_path = files[index];
-					LoadImg();
-					break;
-				}
+				NextImg();
 			}
 			else if (e.KeyCode == Keys.Right)
 			{
-				var files = GetFilesInFolder();
-				var index = Array.IndexOf(files, _path);
-				while (index < files.Length - 1)
-				{
-					index++;
-					var extension = Path.GetExtension(files[index]);
-					if (!allowedExtensions.Contains(extension))
-						continue;
-					_path = files[index];
-					LoadImg();
-					break;
-				}
+				PreviousImg();
+			}
+			else if (e.KeyCode == Keys.F)
+			{
+				_filter = !_filter;
 			}
 			else if (e.KeyCode == Keys.ControlKey)
 			{
 				_slowZoom = true;
+			}
+		}
+
+		private void PreviousImg()
+		{
+			var files = GetFilesInFolder();
+			var index = Array.IndexOf(files, _path);
+			while (index < files.Length - 1)
+			{
+				index++;
+				var extension = Path.GetExtension(files[index]);
+				if (!allowedExtensions.Contains(extension))
+					continue;
+				_path = files[index];
+				LoadImg();
+				break;
+			}
+		}
+
+		private void NextImg()
+		{
+			var files = GetFilesInFolder();
+			var index = Array.IndexOf(files, _path);
+			while (index > 0)
+			{
+				index--;
+				var extension = Path.GetExtension(files[index]);
+				if (!allowedExtensions.Contains(extension))
+					continue;
+				_path = files[index];
+				LoadImg();
+				break;
 			}
 		}
 	}
